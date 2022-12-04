80de7bab8940649fb4a12b27e4ced2f0832ba388
siva-warhammer1998 <67817982+siva-warhammer1998@users.noreply.github.com>
2022-12-04 14:48:38 -0500

    Delete backend.tf
    removed AWS keys

--- backend/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-   backend "s3" {
-     bucket = "terraform-siva-state"
-     key = "terraform/tfstate.tfstate"
-     region = "us-east-1"
-     access_key = "AKIAXL3SCLNTX52VYSG4"
-     secret_key = "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"
-   }
-}
